dcb25b5028694186d72c094cb6cb455a5a56c198
Hessam Imen <hessam.imen@gmail.com>
2022-03-07 21:54:26 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let newArr = [];
+    for(let i=0; i<names.length; i++){
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr;
+}
+// console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+function countDown(num){
+    let i = num;
+    while(i>=0){
+        console.log(i--);
+        
+    }
+}
+// countDown(10);
